2c8286a8b20df4abdda727e18216b8e4203b0b21
Ramiro <alegre.ramiro222@gmail.com>
2021-09-05 19:26:08 -0300

    Países con sus banderas
    Ahora se puede elegir tú país clickeando en su imagen. Se hace uso de una API que provee las imágenes.

---
+++ API.js
@@ -0,0 +1,40 @@
+//--Api para obtener las imagenes de las banderas--//
+const url = 'https://flagcdn.com/es/codes.json';
+
+export const obtenerCodigos = async() => {
+
+    const resultado = await fetch(url);
+    const respuesta = await resultado.json();
+    dibujarBanderas(respuesta);
+}
+
+const dibujarBanderas = (codigos) => {
+    const banderas = document.querySelector('.banderas');
+    for (let i in codigos) {
+        const imagen = document.createElement('img');
+        const input = document.createElement('input');
+
+        //Atributos de los input
+        input.type = 'radio';
+        input.name = 'bandera';
+        input.id = 'flag';
+        input.dataset.code = i;
+
+        //Atributos de las imagenes
+        imagen.src = `https://flagcdn.com/60x45/${i}.png`;
+        imagen.srcset = `https://flagcdn.com/120x90/${i}.png 2x, https://flagcdn.com/180x135/${i}.png 3x`;
+        imagen.width = 60;
+        imagen.height = 45;
+        imagen.classList.add('bandera');
+        imagen.alt = codigos[i];
+        imagen.title = codigos[i];
+        imagen.onclick = () => input.checked = true;
+
+
+        banderas.appendChild(imagen);
+        banderas.appendChild(input);
+    }
+
+
+
+}
--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+import { obtenerCodigos } from './API.js';
+
 //Variables 
 const resultado = document.querySelector('.resultado');
 const formulario = document.querySelector('#formulario');
@@ -7,22 +9,28 @@ const errores = document.querySelector('.errores');
 eventListeners()
 
 function eventListeners() {
+    document.addEventListener('DOMContentLoaded', obtenerCodigos);
     formulario.addEventListener('submit', validarFormulario);
 }
 
 function validarFormulario(e) {
     e.preventDefault();
+    let paisSeleccionado = '';
     const ciudad = document.querySelector('#ciudad');
-    const pais = document.querySelector('#pais');
+    const paises = document.querySelectorAll('#flag').forEach(pais => {
+        if (pais.checked) {
+            paisSeleccionado = pais.dataset.code;
+        }
+    });
 
     //validaciones
 
-    const validar = validaciones(ciudad.value, pais.value);
+    const validar = validaciones(ciudad.value, paisSeleccionado);
     if (validar) {
         //Este html es para el cargador
         let html = `<div class="lds-ellipsis"><div></div><div></div><div></div><div></div></div>`
         resultado.innerHTML = html;
-        obtenerApi(ciudad.value, pais.value);
+        obtenerApi(ciudad.value, paisSeleccionado);
     } else {
         console.log('Algo paso');
     }
